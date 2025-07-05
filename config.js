7113e76b63ccd5ecdde3a1875cabaadba9c892a2
ArslanMD Official <numberkliye@gmail.com>
2025-07-06 00:13:36 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,12 +6,12 @@ module.exports = {
 SESSION_ID: process.env.SESSION_ID || "ARSL~46VGQCQK#KbV-wzJoxhZQJAULvn6431CFqWM479g89t96Tyo-2cg",  // ADD YOUR SESSION ID 
 ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/FLSgNhW9/Free.png",
 ALIVE_MSG: process.env.ALIVE_MSG || "HEY DEAR, I'm thrilled to announce that HansByte Md is ALIVE now—ready to dive into new adventures with you!",
-SUDO_NB: process.env.SUDO_NB || "237696900612",
-AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "false",
+SUDO_NB: process.env.SUDO_NB || "923237045919",
+AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-AUTO_VOICE:"true",
+AUTO_VOICE:"false",
 OMDB_API_KEY: "5e339fb7",
-OWNER_NAME: process.env.OWNER_NAME || "Harold Mth",
-AUTO_TYPING: process.env.AUTO_TYPING || "true",
+OWNER_NAME: process.env.OWNER_NAME || "ArslanMD Official",
+AUTO_TYPING: process.env.AUTO_TYPING || "false",
 ALWAYS_ONLINE: process.env.ALWAYS_ONLINE || "true"
 };
